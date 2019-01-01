c1cee244d93171924a4421af85d54a9943d9b834
Janne Ramstedt <janne.ramstedt@aniway.fi>
2019-01-02 00:58:08 +0200

    loop optimization

--- objects/objects.asm
+++ objects/objects.asm
@@ -3,121 +3,124 @@
 ; width $13F = 319
 ; height $DF = 223
 processObjects
-	move.b	#0, spriteCount
+    move.b	#0, spriteCount
 
-	lea.l	gameObjects, a0
-	move.w	#127,d7		; see memorymap.asm, max 128 game objects
-@loop
-	move.l d7, -(sp)	; push object counter to stack
+    lea.l	gameObjects, a0
+    move.w	#127, d7		; see memorymap.asm, max 128 game objects
 	moveq	#0,	d0
-	move.b	obClass(a0), d0
-	beq.s	@skip
-	lsr.b	#4, d0		; get class from nibble
-	lsl.w	#2, d0		; index to pointer
+@loop
+    move.b	obClass(a0), d0
+    beq.s	@skip
+    
+    lsr.b	#4, d0		; get class from nibble
+    lsl.w	#2, d0		; index to pointer
 
-	movea.l	objectsOrigin-sizeLong(pc,d0.w), a2 ; class start at 1, decrement address by one long
-	jsr		(a2)		; jump to object code
+	move.l	d7, -(sp)	; push object counter to stack
+
+    movea.l	objectsOrigin-sizeLong(pc,d0.w), a2 ; class start at 1, decrement address by one long
+    jsr		(a2)		; jump to object code
+
+	move.l	(sp)+, d7	; pop object counter from stack
+	moveq	#0,	d0
 
 @skip
-	lea	obDataSize(a0), a0
-	move.l	(sp)+, d7	; pop object counter from stack
-	dbra d7, @loop
+    lea	obDataSize(a0), a0
+    dbra d7, @loop
 
-	moveq	#0,	d0
-	move.b	spriteCount, d0
-	beq	@exit
-	lsl	#2, d0	; 4 words per sprite
+    move.b	spriteCount, d0
+    beq	@exit
+    lsl	#2, d0	; 4 words per sprite
 	queueDMATransfer #spriteAttrTable, #vdp_map_sat, d0
 
 @exit
-	moveq	#0,	d0
-	move.b	spriteCount, d0
-	sub #1, d0
-	lsl	#3, d0	; 8bytes per sprite
+    moveq	#0,	d0
+    move.b	spriteCount, d0
+    sub #1, d0
+    lsl	#3, d0	; 8 bytes per sprite
 
-	lea spriteAttrTable, a0
-	add	d0, a0
-	move.b #0, sLinkData(a0)
+    lea spriteAttrTable, a0
+    add	d0, a0
+    move.b #0, sLinkData(a0)
 
-	rts
+    rts
 
 ; input:
 ;	a0 object
 ;	a6 rom address
 displaySprite
-	lea	spriteAttrTable, a2
-	movea.l	a6, a3	; rom address
-	movea.l	(a3), a4		; a4 is patterns start in ROM
+    lea	spriteAttrTable, a2
+    movea.l	a6, a3	; rom address
+    movea.l	(a3), a4		; a4 is patterns start in ROM
 
-	moveq	#0, d0
-	move.b	obAnim(a0), d0	; get animation number
-	and.b	#$F0, d0
-	lsr.b	#4-1, d0	; convert number to offset (word per pointer)
+    moveq	#0, d0
+    move.b	obAnim(a0), d0	; get animation number
+    and.b	#$F0, d0
+    lsr.b	#4-1, d0	; convert number to offset (word per pointer)
 
-	move.w	sizeLong(a3, d0.w), d0	; d0 is offset to metasprite data from rom address
-	lea		(a3, d0.w), a3	; a3 is metasprite data address
+    move.w	sizeLong(a3, d0.w), d0	; d0 is offset to metasprite data from rom address
+    lea		(a3, d0.w), a3	; a3 is metasprite data address
 
-	moveq	#0, d3
-	move.b	obAnim+1(a0), d3
-	and.b	#$3F, d3
-	tst d3
-	beq	@drawSprites
+    moveq	#0, d3
+    move.b	obAnim+1(a0), d3
+    and.b	#$3F, d3
+    tst d3
+    beq	@drawSprites
 
-	subq	#1, d3			; decrement one for loop
+    subq	#1, d3			; decrement one for loop
 @findFrame
-	move.w	(a3)+, d0		; d0 is	sprite count
+    move.w	(a3)+, d0		; d0 is	sprite count
 
-	move.w	d0, d2
-	lsl	#3, d0	; * 8
-	add.w	d2, d2	; * 2
-	add.w	d2, d0	; = * 10 	;mulu	#10, d0
+    move.w	d0, d2
+    lsl	#3, d0	; * 8
+    add.w	d2, d2	; * 2
+    add.w	d2, d0	; = * 10 	;mulu	#10, d0
 
-	adda	d0, a3
-	dbra	d3,	@findFrame
+    adda	d0, a3
+    dbra	d3,	@findFrame
 
 @drawSprites
-	move.w	(a3)+, d0		; d0 is	sprite count
-	subq	#1, d0			; decrement one for loop
+    move.w	(a3)+, d0		; d0 is	sprite count
+    subq	#1, d0			; decrement one for loop
 
-	moveq	#0, d2
-	move.w	obVRAM(a0), d2
-	lsr	#5, d2	;	address to pattern number
+    moveq	#0, d2
+    move.w	obVRAM(a0), d2
+    lsr	#5, d2	;	address to pattern number
 
 @drawSprite
-	addq.b	#1, spriteCount
+    addq.b	#1, spriteCount
 
-	movem.w	(a3)+, d3-d7
+    movem.w	(a3)+, d3-d7
 
-	add	#$EF, d3	; screen center vertical
-	move.b	spriteCount, d4
-	add	d2, d5	; add real VRAM pattern id to dplc relative tile position
-	add	#$11F, d6	; screen center horizontal
+    add	#$EF, d3	; screen center vertical
+    move.b	spriteCount, d4
+    add	d2, d5	; add real VRAM pattern id to dplc relative tile position
+    add	#$11F, d6	; screen center horizontal
 
-	; write to spriteAttrTable
-	move.w	d3, (a2)+
-	move.w	d4, (a2)+
-	move.w	d5, (a2)+
-	move.w	d6, (a2)+
+    ; write to spriteAttrTable
+    move.w	d3, (a2)+
+    move.w	d4, (a2)+
+    move.w	d5, (a2)+
+    move.w	d6, (a2)+
 
-	; TODO move to animation code
-	move.w	d7, d1
-	and.w	#$000F, d1
-	addq	#1, d1
-	lsl	#4, d1	; lsl 5 + lsr 1 = lsl 4. Amount of words for all patterns
+    ; TODO move to animation code
+    move.w	d7, d1
+    and.w	#$000F, d1
+    addq	#1, d1
+    lsl	#4, d1	; lsl 5 + lsr 1 = lsl 4. Amount of words for all patterns
 
-	and.w	#$7FF0, d7
-	add.w	d7, d7	; lsr 4 + lsl 5 = lsl 1. Byte offset to metasprite pattern data
-	lea	(a4, d7), a5	; a4 is ROM address for start of metasprite pattern data
+    and.w	#$7FF0, d7
+    add.w	d7, d7	; lsr 4 + lsl 5 = lsl 1. Byte offset to metasprite pattern data
+    lea	(a4, d7), a5	; a4 is ROM address for start of metasprite pattern data
 
-	lsl	#5, d5	; 32 bytes per pattern
-	move.l d5, a6
+    lsl	#5, d5	; 32 bytes per pattern
+    move.l d5, a6
 
-	queueDMATransfer a5, a6, d1
-	; TODO END
+    queueDMATransfer a5, a6, d1
+    ; TODO END
 
-	dbra	d0, @drawSprite
+    dbra	d0, @drawSprite
 
-	rts
+    rts
 
 ; input:
 ;	a0 object
@@ -125,63 +128,63 @@ displaySprite
 ; trash:
 ;	d0, d1, d2, a5, a6
 animateSprite
-	subq	#1, obFrameTime(a0)
-	bmi		@processAnim
-	rts
+    subq	#1, obFrameTime(a0)
+    bmi		@processAnim
+    rts
 
 @processAnim
-	moveq	#0,	d0
-	move.b	obAnim(a0), d0	; get animation number
-	and.b	#$F0, d0
-	lsr.b	#4-1, d0	; convert number to table offset (word per pointer)
-	move	(a6, d0.w),	d0	; d0 is offset to animation data
-	lea	(a6, d0.w), a6	; a5 is animation script address
+    moveq	#0,	d0
+    move.b	obAnim(a0), d0	; get animation number
+    and.b	#$F0, d0
+    lsr.b	#4-1, d0	; convert number to table offset (word per pointer)
+    move	(a6, d0.w),	d0	; d0 is offset to animation data
+    lea	(a6, d0.w), a6	; a5 is animation script address
 
-	moveq	#0,	d2
-	move.b	(a6)+, d2	; d2 is animation speed
-	
+    moveq	#0,	d2
+    move.b	(a6)+, d2	; d2 is animation speed
+    
 @nextFrame
-	move.w	obAnim(a0), d0
-	move	d0, d1	; d1 is animation data
+    move.w	obAnim(a0), d0
+    move	d0, d1	; d1 is animation data
 
-	and.w	#$0FC0, d0
-	lsr.w	#6, d0	; d0 is animation index
-	move.b (a6, d0.w), d0	; d0 is frame or opcode
-	bmi	@processOpcode
-	
-	add	d2, obFrameTime(a0)
+    and.w	#$0FC0, d0
+    lsr.w	#6, d0	; d0 is animation index
+    move.b (a6, d0.w), d0	; d0 is frame or opcode
+    bmi	@processOpcode
+    
+    add	d2, obFrameTime(a0)
 
-	and.w	#$FFC0, d1
-	or.b	d0, d1
-	add	#64, d1 ; increase animation index
-	move.w	d1, obAnim(a0)
+    and.w	#$FFC0, d1
+    or.b	d0, d1
+    add	#64, d1 ; increase animation index
+    move.w	d1, obAnim(a0)
 
-	rts
+    rts
 
 @processOpcode
-	and.w	#$F03F, d1
-	move.w	d1, obAnim(a0)
-	bra	@nextFrame
+    and.w	#$F03F, d1
+    move.w	d1, obAnim(a0)
+    bra	@nextFrame
 
 
 
 
 deleteObject
-	moveq	#(obDataSize/sizeLong), d0
-	moveq	#0, d1
+    moveq	#(obDataSize/sizeLong), d0
+    moveq	#0, d1
 @loop
-	move.l	d1, (a0)+
-	dbra d0, @loop
-	rts
+    move.l	d1, (a0)+
+    dbra d0, @loop
+    rts
 
 findFreeObject
-	lea.l	gameObjects, a2
-	move.w	#127,d0		; see memorymap.asm, max 128 game objects
+    lea.l	gameObjects, a2
+    move.w	#127,d0		; see memorymap.asm, max 128 game objects
 @loop
-	tst.b	(a2)
-	beq.s	@found
-	lea	obDataSize(a2), a2
-	dbra d0, @loop
-	
+    tst.b	(a2)
+    beq.s	@found
+    lea	obDataSize(a2), a2
+    dbra d0, @loop
+    
 @found
-	rts
+    rts
