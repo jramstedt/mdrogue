66cda5eb2cff0308118278e1dfa380ff61ee446a
Janne Ramstedt <janne.ramstedt@aniway.fi>
2019-01-21 05:06:51 +0200

    correct sintable endianess

--- scripts/sintable.js
+++ scripts/sintable.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 
-const table = new Int16Array(512)
+const table = new DataView(new ArrayBuffer(256*2*2))
 
 for(let i = 0; i < 256; ++i) {
 	let sin = Math.trunc(Math.sin(i/256 * Math.PI*2) * 0x7FFF)
-	if(sin < 0)
-		sin += 0x10000
-
 	let cos = Math.trunc(Math.cos(i/256 * Math.PI*2) * 0x7FFF)
-	if(cos < 0)
-		cos += 0x10000
-
-	table[(i << 1)] = sin
-	table[(i << 1) + 1] = cos
+	
+	table.setInt16(i << 2, sin, false)
+	table.setInt16((i << 2) + 2, cos, false)
 }
 
-fs.writeFileSync('sin.bin', table)
+fs.writeFileSync('assets/sin.bin', table)
