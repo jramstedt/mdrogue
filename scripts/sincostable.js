457834b127fec5cd92c5cb68692acc01d4d180eb
Janne Ramstedt <janne.ramstedt@aniway.fi>
2019-12-23 03:53:08 +0200

    whitespace fix

--- scripts/sincostable.js
+++ scripts/sincostable.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const table = new DataView(new ArrayBuffer(256*2*2))
 
 for(let i = 0; i < 256; ++i) {
-	let sin = Math.trunc(Math.sin(i/256 * Math.PI*2) * 0x7FFF)
-	let cos = Math.trunc(Math.cos(i/256 * Math.PI*2) * 0x7FFF)
-	
-	table.setInt16(i << 2, sin, false)
-	table.setInt16((i << 2) + 2, cos, false)
+  let sin = Math.trunc(Math.sin(i/256 * Math.PI*2) * 0x7FFF)
+  let cos = Math.trunc(Math.cos(i/256 * Math.PI*2) * 0x7FFF)
+  
+  table.setInt16(i << 2, sin, false)
+  table.setInt16((i << 2) + 2, cos, false)
 }
 
 fs.writeFileSync('assets/sincos.bin', table)
